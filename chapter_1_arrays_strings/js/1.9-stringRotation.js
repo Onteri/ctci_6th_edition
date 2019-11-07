fd3bb27b5834f0f2fcead8eec24a13dd4bd85147
Tu <tuvo1106@gmail.com>
2019-11-07 13:48:49 -0800

    Add solution for 1.9

---
+++ chapter_1_arrays_strings/js/1.9-stringRotation.js
@@ -0,0 +1,15 @@
+/* O(n) solution */
+
+/* Determines if a string is a rotation of another string. */
+const isRotation = (str1, str2) => {
+  if (str1.length == str2.length && str1.length) {
+    const str1str1 = str1 + str1
+    return str1str1.includes(str2)
+  }
+  return false
+}
+
+console.log(isRotation('erbottlewat', 'waterbottle')) // true
+console.log(isRotation('erbottlewa', 'waterbottle')) // false
+console.log(isRotation('bot', 'tbo')) // true
+console.log(isRotation('bot', 'tob')) // false
---
+++ chapter_1_arrays_strings/python/1.9-string_rotation.py
@@ -0,0 +1,17 @@
+# O(n) solution
+
+
+def is_rotation(str1, str2):
+    """
+    Determines if a string is a rotation of another string.
+    """
+    if len(str1) == len(str2) and len(str1):
+        str1str1 = str1 * 2
+        return str2 in str1str1
+    return False
+
+
+print(is_rotation("erbottlewat", "waterbottle"))  # true
+print(is_rotation("erbottlewa", "waterbottle"))  # false
+print(is_rotation("bot", "tbo"))  # true
+print(is_rotation("bot", "tob"))  # false
