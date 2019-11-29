6e252e0b4c4606d091d2081e156e76c179f8d987
Tu Vo <tuvo1106@gmail.com>
2019-11-28 20:43:53 -0800

    Rename C files in chapter 5

--- chapter_5_bit_manipulation/c/5.1-insertion.c
+++ chapter_5_bit_manipulation/c/5.1-insertion.c
@@ -1,23 +1,4 @@
-#include <stdio.h>
-
-/* Prints the binary presentation of an integer */
-void bin(int n)
-{
-    int flag = 0;
-    // Size of an integer is assumed to be 32 bits
-    for (int i = 31; i >= 0; i--)
-    {
-        int k = n >> i;
-        if (k & 1)
-        {
-            printf("1");
-            flag = 1;
-        }
-        else if (flag)
-            printf("0");
-    }
-    printf("\n");
-}
+#include "helper.h"
 
 /* Insert m into n at bits i through j */
 int insert_bits(int n, int m, int i, int j)
