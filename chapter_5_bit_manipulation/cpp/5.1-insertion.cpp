864a43aa726694dacce5200ba7979b0011b3f501
Tu Vo <tuvo1106@gmail.com>
2019-11-28 20:44:29 -0800

    Add header for chapter 5 in C++

--- chapter_5_bit_manipulation/cpp/5.1-insertion.cpp
+++ chapter_5_bit_manipulation/cpp/5.1-insertion.cpp
@@ -1,25 +1,4 @@
-#include <iostream>
-
-using namespace std;
-
-/* Prints the binary presentation of an integer */
-void bin(int n)
-{
-  bool flag = false;
-  // Size of an integer is assumed to be 32 bits
-  for (int i = 31; i >= 0; i--)
-  {
-    int k = n >> i;
-    if (k & 1)
-    {
-      cout << "1";
-      flag = true;
-    }
-    else if (flag)
-      cout << "0";
-  }
-  cout << endl;
-}
+#include "helper.h"
 
 /* Insert m into n at bits i through j */
 int insert_bits(int n, int m, int i, int j)
