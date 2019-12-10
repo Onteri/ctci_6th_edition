07947d4f110accbdf51df94fa97e451a916cbf0d
Tu <tuvo1106@gmail.com>
2019-12-10 12:56:44 -0800

    Refactor header file into separate files

--- chapter_4_trees_graphs/c/tree.h
+++ chapter_4_trees_graphs/c/tree.h
@@ -1,10 +1,10 @@
-#ifndef TREE_NODE
-#define TREE_NODE
-
-#define MAX(x, y) ((x) > (y) ? (x) : (y))
+#ifndef TREE_NODE_
+#define TREE_NODE_
 
 #include <stdio.h>
 #include <stdlib.h>
+
+#define MAX(x, y) ((x) > (y) ? (x) : (y))
 
 typedef struct tree_node
 {
@@ -14,71 +14,10 @@ typedef struct tree_node
     struct tree_node *parent;
 } tree_node;
 
-/**
- * Tree Node constructor.
- */
-tree_node *create_node(int value)
-{
-    tree_node *new_node;
-
-    new_node = malloc(sizeof(tree_node));
-    new_node->left = NULL;
-    new_node->right = NULL;
-    new_node->parent = NULL;
-    new_node->value = value;
-    return new_node;
-}
-
-/**
- * Adds a node to the left.
- */
-tree_node *add_left(tree_node *root, int value)
-{
-    tree_node *new_node;
-
-    new_node = create_node(value);
-    new_node->parent = root;
-    root->left = new_node;
-    return new_node;
-}
-
-/**
- * Adds a node to the right.
- */
-tree_node *add_right(tree_node *root, int value)
-{
-    tree_node *new_node;
-
-    new_node = create_node(value);
-    new_node->parent = root;
-    root->right = new_node;
-    return new_node;
-}
-
-/**
- * Returns the height of the tree from the root.
- */
-int find_height(tree_node *root)
-{
-    int left, right;
-
-    if (!root)
-        return 0;
-    left = find_height(root->left);
-    right = find_height(root->right);
-    return MAX(left, right) + 1;
-}
-
-/**
- * Prints current node before child nodes.
- */
-void pre_order_traversal(tree_node *root)
-{
-    if (!root)
-        return;
-    printf("%i ", root->value);
-    pre_order_traversal(root->left);
-    pre_order_traversal(root->right);
-}
+tree_node *create_node(int value);
+tree_node *add_left(tree_node *root, int value);
+tree_node *add_right(tree_node *root, int value);
+int find_height(tree_node *root);
+void pre_order_traversal(tree_node *root);
 
 #endif
