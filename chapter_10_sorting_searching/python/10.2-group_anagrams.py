4099da336d8c702d6a724be0f5e9a93bd2dd7b04
Tu <tuvo1106@gmail.com>
2019-12-16 11:06:03 -0800

    Add solution for 10.2 in Python

---
+++ chapter_10_sorting_searching/python/10.2-group_anagrams.py
@@ -0,0 +1,21 @@
+from typing import List
+
+
+def sort_anagrams(arr: List[str]) -> List[str]:
+    """
+    Sort an array of strings so that all anagrams are next to each other.
+    """
+    hash_table = {}
+    res = []
+    for word in arr:
+        sorted_word = "".join(sorted(word))
+        hash_table.setdefault(sorted_word, []).append(word)
+    for k, v in hash_table.items():
+        for word in v:
+            res.append(word)
+    return res
+
+
+str_list = ["acre", "home", "race", "hack", "cram", "acer"]
+print(sort_anagrams(str_list))
+# ['acre', 'race', 'acer', 'home', 'hack', 'cram']
