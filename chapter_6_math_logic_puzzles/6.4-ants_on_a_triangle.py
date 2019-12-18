39b3d75ad3dce6bceb22b94230637e92c148b42a
Tu <tuvo1106@gmail.com>
2019-12-17 18:00:46 -0800

    Update documentation

--- chapter_6_math_logic_puzzles/6.4-ants_on_a_triangle.py
+++ chapter_6_math_logic_puzzles/6.4-ants_on_a_triangle.py
@@ -5,7 +5,7 @@ P(same direction) = (1/2)**3 + (1/2)**3 = 1/4
 The probability of collision is therefore the probability of the ants not
 moving in the same direction.
 P(collision) = 1 - P(samedirection) = 1 - 1/4 = 3/4
-To generaliuze this to an n-vertex polygon, there are still only two ways in
+To generalize this to an n-vertex polygon, there are still only two ways in
 which the ants can move to avoid collsion, but there are 2**n ways they can
 move in total.
 P(clockwise) = (1/2)**n
