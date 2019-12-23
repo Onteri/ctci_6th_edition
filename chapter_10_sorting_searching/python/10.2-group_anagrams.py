1f57ee0c369b07a0b35545c87c4045a41891741a
Tu Vo <tuvo1106@gmail.com>
2019-12-23 13:48:36 -0800

    Refactor solution for 10.2

--- chapter_10_sorting_searching/python/10.2-group_anagrams.py
+++ chapter_10_sorting_searching/python/10.2-group_anagrams.py
@@ -6,14 +6,11 @@ def sort_anagrams(arr: List[str]) -> List[str]:
     Sorts an array of strings so that all anagrams are next to each other.
     """
     hash_table = {}
-    res = []
     for word in arr:
         sorted_word = "".join(sorted(word))
         hash_table.setdefault(sorted_word, []).append(word)
-    for k, v in hash_table.items():
-        for word in v:
-            res.append(word)
-    return res
+    # List comprehension to flatten list.
+    return [v for s in hash_table.values() for v in s]
 
 
 str_list = ["acre", "home", "race", "hack", "cram", "acer"]
